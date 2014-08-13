54787435e3d89fe21a55113432c59358e08ee4f9
Max Branzburg <maxbranzburg@gmail.com>
2014-08-13 00:04:50 -0700

    Committing Teddit conditionals homework

---
+++ MaxBranzburg/ex_teddit_conditional.rb
@@ -0,0 +1,50 @@
+# Teddit Contionals - Starter Code.
+
+# Let's add a new Teddit feature. Upvotes!
+# Complete the application below. 
+# Where you see comments (lines that begin with #) replace it with code so that the program works.
+
+def get_input
+  #Get input from the user.
+  input = gets.chomp
+end
+
+def calculate_upvotes(story, category)
+	# Write code so that:
+		# If the Story is about cats multiply the upvotes by 5
+		# If the Story is about bacon multiply the upvotes by 8
+		# If the Story is about Food it gets 3 times the upvotes.
+		upvotes ||= 1
+
+		if category.downcase.to_s == "cats"
+			upvotes = upvotes * 5
+		
+		
+		elsif category.downcase.to_s == "bacon"
+			upvotes = upvotes * 8
+		
+
+		elsif category.downcase.to_s == "food"
+			upvotes = upvotes * 3
+		
+		else upvotes 
+
+		end
+
+	#For example:
+	# "Cats frolic despite tuna shortage" should give you 5 times the upvotes!
+end
+
+puts "Welcome to Teddit! a text based news aggregator. Get today's news tomorrow!"
+puts "Please enter a News story:"
+story = get_input
+puts "Please give it a category:"
+category = get_input
+upvotes = calculate_upvotes(story, category)
+puts "New story added! \nStory: \t\t\t#{story} \nCategory: \t\t#{category} \nCurrent Upvotes: \t#{upvotes}"
+
+
+
+
+
+
