ee73b8619c965d0c7be506da673f45d87115d80a
T Humphrey <thumphrey3@users.noreply.github.com>
2014-08-19 21:12:52 -0700

    Second Quiz inside THump folder

---
+++ TolisonHumphrey-thump/05_starter_code/ex_apartment_objects/lib/building.rb
@@ -0,0 +1,11 @@
+#Building Class
+class Building
+	attr_accessor :building_name, :building_address
+	def initialize (building_name, building_address)
+		@building_name = building_name
+		@building_address = building_address
+
+		@apt_count = 0
+	end
+
+	
