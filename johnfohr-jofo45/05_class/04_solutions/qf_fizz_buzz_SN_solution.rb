9508f47c4503e9a758c1ee574ef0d63e92ff9674
John Fohr <jofo45@gmail.com>
2014-08-19 21:31:16 -0700

    correction for homework at 05 class

---
+++ johnfohr-jofo45/05_class/04_solutions/qf_fizz_buzz_SN_solution.rb
@@ -0,0 +1,37 @@
+# Write a program that prints the numbers from 1 to 100. 
+# But for multiples of three print “Fizz” instead of the number and for the multiples of five print “Buzz”. 
+# For numbers which are multiples of both three and five print “FizzBuzz”.
+#
+# Your code here
+
+def fizzbuzz(number)
+  result = ''
+  result << 'Fizz' if number % 3 == 0
+  result << 'Buzz' if number % 5 == 0
+  return result unless result.empty?
+  number
+end
+
+
+
+# =============================================================================
+# DO NOT EDIT BELOW THIS LINE!
+# =============================================================================
+
+# method to check for equality
+def assert_equal(expected, actual)
+  raise "Expected #{expected.nil? and 'nil' or expected} to match #{actual}" unless expected == actual
+end
+
+# Tests
+assert_equal fizzbuzz(1), 1
+assert_equal fizzbuzz(3), "Fizz"
+assert_equal fizzbuzz(4), 4
+assert_equal fizzbuzz(50), "Buzz"
+assert_equal fizzbuzz(15), "FizzBuzz"
+assert_equal fizzbuzz(5175), "FizzBuzz"
+
+# Output!
+1.upto(100) do |number|
+  puts fizzbuzz(number)
+end
---
+++ johnfohr-jofo45/05_class/starter_code/ex_apartment_objects/lib/building.rb
@@ -0,0 +1,14 @@
+#Building Class
+class Building
+	attr_accessor :building_name, :building_address
+
+		def initialize(building_name, building_address)
+			@building_name = building_name
+			@building_address  = building_address
+		end
+
+	def apartments(unit)
+		apartments = {}
+	end
+
+end
